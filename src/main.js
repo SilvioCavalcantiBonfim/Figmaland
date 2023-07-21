7e9f0aaff4ee035884a4d33e9a2c4aa816a0684a
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2023-07-21 11:48:40 -0300

    1148

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const series = [new Serie('BRIDGERTON', 0.98, '/assets/bg1.jpeg')];
+var currentSerie = series[0];
+
+
+function onInit() {
+  // background
+  const background = document.getElementById('background');
+  background.style.background = `url(${currentSerie.img}) lightgray 50% / cover no-repeat`;
+}
