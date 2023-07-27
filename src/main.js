558561af727972c3bf39c6b3715fb33b394add64
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2023-07-27 17:17:30 -0300

    2707

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-const series = [new Serie('BRIDGERTON', 0.98, '/assets/bg1.jpeg')];
-var currentSerie = series[0];
+function init() {
+  const video = document.getElementById('video__1');
+  const bt_video = document.getElementById('button__play__1');
+  video.addEventListener('ended',() => {
+    bt_video.classList.remove("d-none");
+  })
 
-
-function onInit() {
-  // background
-  const background = document.getElementById('background');
-  background.style.background = `url(${currentSerie.img}) lightgray 50% / cover no-repeat`;
-}
-
-function focusIn(id) {
-  document.getElementById(id).focus();
+  bt_video.addEventListener('click', () => {
+    if (video.ended) video.currentTime = 0;
+    bt_video.classList.add("d-none");
+    video.play();
+  })
 }
