452322723732a8147a8151ac7d9fcf68b7577cdf
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2023-07-28 11:59:20 -0300

    2807

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 function init() {
   const video = document.getElementById('video__1');
   const bt_video = document.getElementById('button__play__1');
+
   video.addEventListener('ended',() => {
     bt_video.classList.remove("d-none");
+  })
+
+  video.addEventListener('click', () => {
+    bt_video.classList.remove("d-none");
+    video.pause();
   })
 
   bt_video.addEventListener('click', () => {
